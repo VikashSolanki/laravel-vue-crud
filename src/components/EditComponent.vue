5a971616dd7a574e30f6695597f940cf4ccac77e
vikash <vikram.solanki@multidots.com>
2019-04-15 17:56:25 +0530

    update the post data

--- src/components/EditComponent.vue
+++ src/components/EditComponent.vue
@@ -1,21 +1,65 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Edit Component</div>
-
-              <div class="card-body">
-                  I'm an Edit component.
-              </div>
+  <div>
+    <h1>Edit Post</h1>
+    <form @submit.prevent="updatePost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Title:</label>
+            <input type="text" class="form-control" v-model="post.name">
           </div>
+        </div>
       </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Body:</label>
+            <textarea class="form-control" v-model="post.description" rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+      <br>
+      <div class="form-group">
+        <button class="btn btn-primary">Update</button>
+      </div>
+    </form>
   </div>
 </template>
 
+
 <script>
-    export default {
-        mounted() {
-            console.log('Example Component mounted.')
-        }
+export default {
+  data() {
+    return {
+      post: []
+    };
+  },
+  created() {
+    let URL = `http://127.0.0.1:8000/api/show/${this.$route.params.id}`;
+    this.axios
+      .get(URL)
+      .then(response => {
+        console.log(response.data.data.name);
+        this.post = response.data.data;
+      })
+      .catch(function() {});
+  },
+  methods: {
+    updatePost() {
+      let URL = `http://127.0.0.1:8000/api/update/${this.$route.params.id}`;
+      let loader = this.$loading.show();
+      this.axios
+        .post(URL, this.post)
+        .then(response => {
+          this.post = response.data.data;
+          this.$router.push({ name: "posts" });
+          this.flashSuccess("Post is updated successfully");
+          loader.hide();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  }
+};
 </script>
